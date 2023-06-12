0a322c073252f33632703a7dff7d5367575ed031
maximus <1091261998@qq.com>
2023-06-12 23:54:39 +0800

    异常取消context

--- chromeDp/fetchByChromeDp.go
+++ chromeDp/fetchByChromeDp.go
@@ -2,7 +2,6 @@ package chromeDp
 
 import (
 	"context"
-	"fmt"
 	"github.com/chromedp/chromedp"
 	"github.com/sirupsen/logrus"
 	"log"
@@ -25,6 +24,7 @@ func GetHttpHtmlContent(url string, selector string, sel interface{}) (string, e
 
 	// create context
 	chromeCtx, cancel := chromedp.NewContext(c, chromedp.WithLogf(log.Printf))
+	defer cancel()
 	// 执行一个空task, 用提前创建Chrome实例
 	chromedp.Run(chromeCtx, make([]chromedp.Action, 0, 1)...)
 
@@ -39,33 +39,35 @@ func GetHttpHtmlContent(url string, selector string, sel interface{}) (string, e
 		chromedp.OuterHTML(sel, &htmlContent, chromedp.ByJSPath),
 	)
 
-	select {
-	case <-timeoutCtx.Done(): // 因为ctx带超时参数，当时间期限到了之后就会走到这里退出协程
-		//if err != nil {
-		//fmt.Errorf("请求异常", err)
-		//e := chromedp.Cancel(timeoutCtx)
-		//t, _ := timeoutCtx.Deadline()
-		//logu.Logger.WithFields(logrus.Fields{"time": t})
-		//cancel()
-		//logu.Logger.WithFields(logrus.Fields{"info": "关闭context异常"}).Error(e)
-
-		logu.Logger.WithFields(logrus.Fields{"定位": "chromeDp请求异常"}).Error(err)
-
-		//}
-		fmt.Println("Done. ", time.Now().Format("2006-01-02 15:04:05"))
-		chromedp.Cancel(timeoutCtx)
-		cancel()
-		return "", err
-
-	default: // 协程循环执行for，当ctx.Done()无信号时总是走到Default分支
-		//fmt.Println("case default ", time.Now().Format("2006-01-02 15:04:05"))
-		//time.Sleep(time.Second)
-
-	}
+	//select {
+	//case <-timeoutCtx.Done(): // 因为ctx带超时参数，当时间期限到了之后就会走到这里退出协程
+	//	//if err != nil {
+	//	//fmt.Errorf("请求异常", err)
+	//	//e := chromedp.Cancel(timeoutCtx)
+	//	//t, _ := timeoutCtx.Deadline()
+	//	//logu.Logger.WithFields(logrus.Fields{"time": t})
+	//	//cancel()
+	//	//logu.Logger.WithFields(logrus.Fields{"info": "关闭context异常"}).Error(e)
+	//
+	//	logu.Logger.WithFields(logrus.Fields{"定位": "chromeDp请求异常"}).Error(err)
+	//
+	//	//}
+	//	fmt.Println("Done. ", time.Now().Format("2006-01-02 15:04:05"))
+	//	//chromedp.Cancel(timeoutCtx)
+	//	cancel()
+	//	return "", err
+	//
+	//default: // 协程循环执行for，当ctx.Done()无信号时总是走到Default分支
+	//	//fmt.Println("case default ", time.Now().Format("2006-01-02 15:04:05"))
+	//	//time.Sleep(time.Second)
+	//
+	//}
 
 	if err != nil {
-		//chromedp.Cancel(timeoutCtx)
-		//logu.Logger.WithFields(logrus.Fields{"定位": "chromeDp请求异常"}).Error(err)
+		cancel()
+		e := chromedp.Cancel(timeoutCtx)
+		logu.Logger.WithFields(logrus.Fields{"定位": "chromeDp请求异常"}).Error(err)
+		logu.Logger.Error(e)
 		return "", err
 	}
 
